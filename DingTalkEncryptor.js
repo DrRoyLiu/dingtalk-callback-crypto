22a3fa4cb659e0e8521615b13de19645831cf243
Roy Liu <liup1@qq.com>
2022-03-01 16:56:44 +0800

    修正方法BUG，已通过钉钉“事件订阅”回传success成功
    1.删除了UTF-8和base64的变量
    2.删除了this.keySpec
    3.方法内的const全部改为let
    4.class初始化时，this.aesKey 不需要 toString
    5.encrypt方法中，不需要组装crypto.final()
    6.decrypt方法中，不需要组装crypto.final()
    7.decrypt方法中，pad的计算方法有误，不应通过最后一个buffer确定原文长度，应该通过计算textLen和固定字符数量计算

--- DingTalkEncryptor.js
+++ DingTalkEncryptor.js
@@ -6,16 +6,13 @@ const DingTalkEncryptException = require('./DingTalkEncryptException');
 
 class DingTalkEncryptor {
   constructor(token, encodingAesKey, corpIdOrSuiteKey) {
-    this.utf8 = 'utf-8';
-    this.base64 = 'base64';
     this.AES_ENCODE_KEY_LENGTH = 43;
     this.RANDOM_LENGTH = 16;
 
     this.token = token;
     this.encodingAesKey = encodingAesKey;
-    this.aesKey = (Buffer.from(encodingAesKey + '=', 'base64')).toString();
+    this.aesKey = Buffer.from(encodingAesKey + "=", 'base64');
     this.corpId = corpIdOrSuiteKey;
-    this.keySpec = this.aesKey;
     this.iv = this.aesKey.slice(0, 16);
   }
 
@@ -28,16 +25,16 @@ class DingTalkEncryptor {
 
   encrypt(random, plainText) {
     try {
-      const randomBuf = Buffer.from(random);
-      const plainTextBuf = Buffer.from(plainText);
-      const textLen = plainTextBuf.length;
-      const textLenBuf = Buffer.from([(textLen >> 24 & 255), (textLen >> 16 & 255), (textLen >> 8 & 255), (textLen & 255)]);
-      const cropIdBuf = Buffer.from(this.corpId);
-      const padCount = 32 - (randomBuf.length + textLenBuf.length + plainTextBuf.length + cropIdBuf.length) % 32;
-      const padBuf = Buffer.from(new Array(padCount).fill(padCount));
-      const finalBuf = Buffer.concat([randomBuf, textLenBuf, plainTextBuf, cropIdBuf, padBuf]);
-      const crypto = Crypto.createCipheriv('AES-256-CBC', this.keySpec, this.iv);
-      return Buffer.concat([crypto.update(finalBuf), crypto.final()]).toString('base64');
+      let randomBuf = Buffer.from(random);
+      let plainTextBuf = Buffer.from(plainText);
+      let textLen = plainTextBuf.length;
+      let textLenBuf = Buffer.from([(textLen >> 24 & 255), (textLen >> 16 & 255), (textLen >> 8 & 255), (textLen & 255)]);
+      let corpIdBuf = Buffer.from(this.corpId);
+      let padCount = 32 - (randomBuf.length + textLenBuf.length + plainTextBuf.length + corpIdBuf.length) % 32;
+      let padBuf = Buffer.from(new Array(padCount).fill(padCount));
+      let finalBuf = Buffer.concat([randomBuf, textLenBuf, plainTextBuf, corpIdBuf, padBuf]);
+      let crypto = Crypto.createCipheriv('AES-256-CBC', this.aesKey, this.iv);
+      return Buffer.concat([crypto.update(finalBuf)]).toString('base64');
     } catch (e) {
       throw new DingTalkEncryptException(900007);
     }
@@ -47,38 +44,35 @@ class DingTalkEncryptor {
     let decrypt;
     try {
       // decrypt
-      const crypto = Crypto.createDecipheriv('AES-256-CBC', this.keySpec, this.iv);
-      decrypt = Buffer.concat([crypto.update(encrypted, 'base64'), crypto.final()]);
+      let crypto = Crypto.createDecipheriv('AES-256-CBC', this.aesKey, this.iv);
+      decrypt = Buffer.concat([crypto.update(encrypted, 'base64')]);
     } catch (e) {
       throw new DingTalkEncryptException(900008);
     }
 
-    let cropId;
-    let plainText;
+    let corpId,
+      plainText;
     try {
-      let pad = decrypt[decrypt.length - 1];
-      if (pad < 1 || pad > 32) pad = 0;
-      const finalDecrypt = decrypt.slice(0, decrypt.length - pad);
-      // const random = finalDecrypt.slice(0, 16);
-      const textLen = finalDecrypt.slice(16, 20).readUInt32BE();
-      plainText = finalDecrypt.slice(20, 20 + textLen).toString();
-      cropId = finalDecrypt.slice(20 + textLen).toString();
+      let textLen = decrypt.slice(16, 20).readUInt32BE();
+      plainText = decrypt.slice(20, 20 + textLen).toString();
+      let pad = decrypt.length - 16 - 4 - textLen - 20;
+      if (pad > 31) pad = 0;
+      let finalDecrypt = decrypt.slice(0, decrypt.length - pad);
+      corpId = finalDecrypt.slice(20 + textLen).toString();
     } catch (e) {
       throw new DingTalkEncryptException(900009);
     }
-
-    if (cropId !== this.corpId) {
+    if (corpId != this.corpId) {
       throw new DingTalkEncryptException(900010);
     } else {
       return plainText;
     }
   }
-
   getSignature(token, timestamp, nonce, encrypt) {
     timestamp = timestamp + '';
-    const strArr = [this.token, timestamp, nonce, encrypt];
+    let strArr = [token, timestamp, nonce, encrypt];
     strArr.sort();
-    const sha1 = Crypto.createHash('sha1');
+    let sha1 = Crypto.createHash('sha1');
     sha1.update(strArr.join(''))
     return sha1.digest('hex');
   }
@@ -92,8 +86,8 @@ class DingTalkEncryptor {
     } else if (nonce == null) {
       throw new DingTalkEncryptException(900003);
     } else {
-      const encrypt = this.encrypt(this.getRandomStr(this.RANDOM_LENGTH), plaintext);
-      const signature = this.getSignature(this.token, timeStamp, nonce, encrypt);
+      let encrypt = this.encrypt(this.getRandomStr(this.RANDOM_LENGTH), plaintext);
+      let signature = this.getSignature(this.token, timeStamp, nonce, encrypt);
       return {
         msg_signature: signature,
         encrypt: encrypt,
@@ -104,7 +98,7 @@ class DingTalkEncryptor {
   }
 
   getDecryptMsg(msgSignature, timeStamp, nonce, encryptMsg) {
-    const signature = this.getSignature(this.token, timeStamp, nonce, encryptMsg);
+    let signature = this.getSignature(this.token, timeStamp, nonce, encryptMsg);
     if (signature !== msgSignature) {
       throw new DingTalkEncryptException(900006);
     } else {
@@ -113,7 +107,7 @@ class DingTalkEncryptor {
   }
 
   getRandomStr(size) {
-    const base = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    let base = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
     let randomStr = '';
     for (let i = size; i > 0; --i) {
       randomStr += base[Math.floor(Math.random() * base.length)];
